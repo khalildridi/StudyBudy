fd20309348331c5e000a02565749c8b586b6d411
khalildridi <86881343+khalildridi@users.noreply.github.com>
2022-04-29 01:43:43 +0200

    Register Functionality

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/',views.createRoom,name="create-room"),
+    path('register/', views.registerPage, name="register"),
     path('update-room/<str:pk>',views.updateRoom,name="update-room"),
      path('delete-room/<str:pk>',views.deleteRoom,name="delete-room"),
      path('login/', views.loginPage, name="login"),
