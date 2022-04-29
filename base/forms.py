9cffae721eb6370102afcea3ea454db334e2fa1a
elabed chahine <elabed.chahine@gmail.com>
2022-04-29 02:51:48 +0100

    Updated_ModelForms

--- base/forms.py
+++ base/forms.py
@@ -1,15 +1,16 @@
-import imp
-from importlib.metadata import files
-from django.forms import ModelForm 
-from .models import Room 
+from django.forms import ModelForm
+from .models import Room
 from django.contrib.auth.models import User
+
+
 class RoomForm(ModelForm):
     class Meta:
-        model=Room 
-        fields='__all__'
+        model = Room
+        fields = '__all__'
         exclude = ['host', 'participants']
-    
+
+
 class UserForm(ModelForm):
     class Meta:
-        model =User
+        model = User
         fields = ['username', 'email']
